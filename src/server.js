bc92b2d0a3505e1cb578be76fcecf755431e1354
Matheus Couto <matheuscouto.tds2.senai@gmail.com>
2025-04-15 16:12:28 -0300

    feat: criação dos metodos, esta dando erro

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import avaliationController from "./controllers/avaliationController.js";
+
 const app = express();
 const port = 4000;
+
 app.use(express.json());
 app.use("/avaliacoes", avaliationController);
 app.listen(port, () => {
